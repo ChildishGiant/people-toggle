9736ced39c7feb44333bec6a2d4b65a66d97a628
SolarPolarMan <solarpolarman@gmail.com>
2016-06-04 22:31:16 +0100

    visual & topography redo

--- jssource.js
+++ jssource.js
@@ -1,15 +1,22 @@
 var withTasks = ["get married","eat a meal with people"];
 
-var withoutTasks = ["frolick in public parks naked"];
+var withoutTasks = ["frolick in public parks naked",
+"dress how you like without being judged",];
 
 
 function gen(){
 
   if ($("#switch").is(':checked')) {
-    // Checked
+    // Humanity on
+    $("body").css("background-color", "#d7d7d7");
+    $("body").css("color", "#424242");
+    $("a").css("color", "#424242");
     $("#task").text(withTasks[Math.floor(Math.random() * withTasks.length)].concat("."));
   } else {
-    // Unchecked
+    // Humanity off
+    $("body").css("background-color", "#424242");
+    $("body").css("color", "#d7d7d7");
+    $("a").css("color", "#d7d7d7");
     $("#task").text(withoutTasks[Math.floor(Math.random() * withoutTasks.length)].concat("."));
   };
 };
